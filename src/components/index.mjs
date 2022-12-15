a0c6cc2ec90f6039d1dfd8fb97fe8c7e0351fd1f
Vesta-nna <anna.dominguez.anna@gmail.com>
2022-12-15 22:46:08 +0100

    resolve errors

---
+++ src/components/index.mjs
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from './Header.mjs'
+import Main from './Main.mjs'
+import Footer from './Footer.mjs'
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+)
+
+export default App
